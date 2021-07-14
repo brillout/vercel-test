49f7a46426d5af809edb8b49d4ec0e3117625b83
Romuald Brillout <git@brillout.com>
2021-07-14 20:44:26 +0200

    wip

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+
+var walk = function(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            /* Recurse into a subdirectory */
+            results = results.concat(walk(file));
+        } else {
+            /* Is a file */
+            results.push(file);
+        }
+    });
+    return results;
+}
+
+console.log('__dirname', __dirname)
+const files = walk(path.join(__dirname, '..')).filter(p => !p.includes('node_modules'))
+console.log('__dirname/**/*', files)
+
+module.exports = async (req, res) => {
+  res
+    .status(200)
+    .setHeader("content-type", "text/html")
+    .send("Server rendered HTML");
+};
