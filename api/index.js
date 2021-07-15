aa2674b8b00a6d1f1c72a17d7f66047a277f01e2
Romuald Brillout <git@brillout.com>
2021-07-15 08:54:04 +0200

    wip

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 const path = require("path");
 const fs = require("fs");
 
-console.log("__dirname", __dirname);
-const files = ls(path.join(__dirname, ".."));
-console.log("__dirname/../*", files);
+logFiles();
 
 module.exports = async (req, res) => {
+  logFiles();
   res
     .status(200)
     .setHeader("content-type", "text/html")
     .send("Server rendered HTML");
 };
+
+function logFiles() {
+  console.log("__dirname", __dirname);
+  const files = ls(path.join(__dirname, ".."));
+  console.log("__dirname/../*", files);
+}
 
 function ls(dir) {
   return fs.readdirSync(dir).map(function (file) {
