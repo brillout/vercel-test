bfa4f4f94457eb91d0df2ccf4d0c07f13414f615
Romuald Brillout <git@brillout.com>
2021-07-15 08:49:30 +0200

    wip

--- api/index.js
+++ api/index.js
@@ -1,36 +1,20 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-var walk = function(dir) {
-    var results = [];
-    var list = fs.readdirSync(dir);
-    list.forEach(function(file) {
-        file = dir + '/' + file;
-        var stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) {
-            /* Recurse into a subdirectory */
-            results = results.concat(walk(file));
-        } else {
-            /* Is a file */
-            results.push(file);
-        }
-    });
-    return results;
-}
-
-console.log(0)
-console.log('__dirname', __dirname)
-const files = walk(path.join(__dirname, '..')).filter(p => !p.includes('node_modules'))
-console.log('__dirname/**/*', files)
+console.log("__dirname", __dirname);
+const files = ls(path.join(__dirname, ".."));
+console.log("__dirname/../*", files);
 
 module.exports = async (req, res) => {
-  console.log(4)
-  console.log('__dirname', __dirname)
-  const files = walk(path.join(__dirname, '..')).filter(p => !p.includes('node_modules'))
-  console.log('__dirname/**/*', files)
-
   res
     .status(200)
     .setHeader("content-type", "text/html")
     .send("Server rendered HTML");
 };
+
+function ls(dir) {
+  return fs.readdirSync(dir).map(function (file) {
+    file = dir + "/" + file;
+    return file;
+  });
+}
